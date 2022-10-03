694dfbb2c1a9c2ed91a3f48e467fe86f513be767
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-10-03 02:16:56 -0300

    #1-release: implementando classe Loops

--- factories/ProgramFactory.py
+++ factories/ProgramFactory.py
@@ -3,20 +3,25 @@ from models.MngFiles import MngFiles
 from models.MathOps import MathOps
 from models.ListOps import ListOps
 from models.ListMethods import ListMethods
+from models.Loops import Loops
+
 class ProgramFactory:
     def __init__(self, classes='all'):
         self.classes = classes
     def start(self):
         if self.classes == 'all':
+            acessories = [
+                'Rodas de liga',
+                'Travas elétricas',
+                'Piloto automático',
+                'Bancos de couro',
+                'Ar condicionado',
+            ]
             p = Program([ MngFiles(),
                         MathOps(),
                         ListOps(),
-                        ListMethods([
-                            'Rodas de liga',
-                            'Travas elétricas',
-                            'Piloto automático',
-                            'Bancos de couro',
-                            'Ar condicionado']) ])
+                        ListMethods(acessories),
+                        Loops(acessories) ])
             p.start()
         else:
             p = Program(self.classes)
